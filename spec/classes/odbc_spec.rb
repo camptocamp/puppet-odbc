eb2d24a7bec115323dd2c7142cbfe19e153d3e29
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-12-26 11:02:12 +0100

    Use rspec-puppet-facts

--- spec/classes/odbc_spec.rb
+++ spec/classes/odbc_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
 describe 'odbc' do
-  on_puppet_supported_platforms.each do |version, platforms|
-    platforms.each do |platform, facts|
-      context "on #{version} #{platform}" do
-        let (:facts) { facts }
 
-        it { should compile.with_all_deps }
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts
       end
+
+      it { should compile.with_all_deps }
     end
   end
 end
